d8daa5e00790527df207bea2c7968bfc7160bbf6
ha22119 <55187236+ha22119@users.noreply.github.com>
2020-07-01 09:21:47 +0900

    주석추가
    실험용임

--- app/src/main/java/com/example/likedislike/MainActivity.java
+++ app/src/main/java/com/example/likedislike/MainActivity.java
@@ -11,7 +11,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        //실험
+        // HSM에서 수정
 
     }
 }
