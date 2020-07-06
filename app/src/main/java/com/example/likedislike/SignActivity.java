1eca01511cafe54f0f475a5be55608dd56c05930
ha22119 <55187236+ha22119@users.noreply.github.com>
2020-07-07 00:59:14 +0900

    2020.07.06
    회원가입 구현 끝

---
+++ app/src/main/java/com/example/likedislike/SignActivity.java
@@ -0,0 +1,194 @@
+package com.example.likedislike;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseAuthMultiFactorException;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.MultiFactorResolver;
+
+public class SignActivity extends AppCompatActivity {
+
+    EditText editEmail, editPwd,reEditPwd;
+    Button verifyEmailButton, signUpBtn;
+
+    final static String TAG = "Yahoo";
+
+    private FirebaseAuth mAuth;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign);
+
+        editEmail = findViewById(R.id.editEmail);
+        editPwd = findViewById(R.id.editPwd);
+        reEditPwd = findViewById(R.id.reEditPwd);
+
+//        signInBtn = findViewById(R.id.signInBtn);
+//        signOutButton = findViewById(R.id.signOutButton);
+        verifyEmailButton = findViewById(R.id.verifyEmailButton);
+        signUpBtn =findViewById(R.id.signUpBtn);
+
+        mAuth = FirebaseAuth.getInstance();
+
+
+
+        verifyEmailButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                sendEmailVerification();
+            }
+        });
+        signUpBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                createAccount(editEmail.getText().toString(), editPwd.getText().toString());
+            }
+        });
+
+        //        signInBtn.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                loginAccount(editEmail.getText().toString(), editPwd.getText().toString());
+//            }
+//        });
+//        signOutButton.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                signOut();
+//            }
+//        });
+
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+    }
+
+    private void loginAccount(String email, String password) { // 로그인
+        Log.d(TAG, "loginAccount:" + email);
+        if (!validateForm()) {
+            return;
+        }
+        mAuth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            Log.d(TAG, "signInWithEmail:성공");
+                            FirebaseUser user = mAuth.getCurrentUser();
+                        } else {
+                            Log.w(TAG, "signInWithEmail:실패", task.getException());
+                            Toast.makeText(SignActivity.this, "인증 실패",
+                                    Toast.LENGTH_SHORT).show();
+                        }
+
+                    }
+                });
+    }
+
+    private void createAccount(String email, String password) {
+        Log.d(TAG, "createAccount:" + email);
+        if (!validateForm()) {
+            return;
+        }
+
+        mAuth.createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            Log.d(TAG, "createUserWithEmail:success");
+                            FirebaseUser user = mAuth.getCurrentUser();
+                            Toast.makeText(SignActivity.this, "회원가입 성공",
+                                    Toast.LENGTH_SHORT).show();
+
+                        } else {
+                            Log.w(TAG, "createUserWithEmail:failure", task.getException());
+                            Toast.makeText(SignActivity.this, "회원가입 실패",
+                                    Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+    }
+
+    private void signOut() {
+        mAuth.signOut();
+        Log.d(TAG,"로그아웃 함");
+        Toast.makeText(SignActivity.this, "로그아웃 됨",
+                Toast.LENGTH_SHORT).show();
+        Intent LogOutIntent = new Intent(getApplicationContext(),LoginActivity.class);
+        startActivity(LogOutIntent);
+    }
+
+    private void sendEmailVerification() { // 이메일 인증 보내기
+        verifyEmailButton.setEnabled(false);
+
+        final FirebaseUser user = mAuth.getCurrentUser();
+        user.sendEmailVerification()
+                .addOnCompleteListener(this, new OnCompleteListener<Void>() {
+                    @Override
+                    public void onComplete(@NonNull Task<Void> task) {
+                        verifyEmailButton.setEnabled(true);
+                        Log.d(TAG,"메일 보낼까 말까");
+
+                        if (task.isSuccessful()) {
+                            Toast.makeText(SignActivity.this,
+                                    "LiKEDiSLiKE 에서 \n"+ user.getEmail()+" 로 이메일을 발송하였습니다.",
+                                    Toast.LENGTH_SHORT).show();
+                            Log.d(TAG,"메일 보냄");
+                            finish();
+
+
+                        } else {
+                            Log.d(TAG, "sendEmailVerification", task.getException());
+                            Toast.makeText(SignActivity.this,
+                                    "이메일을 보내는데 실패하였습니다.\n이메일이 정확한지 확인해주세요.",
+                                    Toast.LENGTH_SHORT).show();
+                            Log.d(TAG,"메일 안 보냄");
+
+                        }
+                    }
+                });
+    }
+
+    private boolean validateForm() { // 이게 형식이 맞는지 확인하기
+        boolean valid = true;
+
+        String email = editEmail.getText().toString();
+        if (TextUtils.isEmpty(email)) { // 이메일이 비어있으면 false 반환
+            valid = false;
+        } else {
+        }
+
+        String password = editPwd.getText().toString();
+        String rePassword = reEditPwd.getText().toString();
+        if (TextUtils.isEmpty(password)) {
+            valid = false;
+            Toast.makeText(getApplicationContext(), "비밀번호가 입력되지 않았습니다.", Toast.LENGTH_SHORT).show();
+        } else if(TextUtils.isEmpty(rePassword)){
+            valid = false;
+            Toast.makeText(getApplicationContext(), "비밀번호 확인을 위해 비밀번호를 \n한 번 더 입력해주세요.", Toast.LENGTH_SHORT).show();
+        }else if(!(password.equals(rePassword))) {
+            Toast.makeText(getApplicationContext(), "재입력된 비밀번호와 \n비밀번호가 일치하지 않습니다.", Toast.LENGTH_SHORT).show();
+        }else {
+        }
+        return valid;
+    }
+}
