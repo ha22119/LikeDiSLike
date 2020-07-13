e936f0d7a00eaa08d1d786374d5d976d76f6ff0f
onebii <48761411+onebii@users.noreply.github.com>
2020-07-13 19:56:06 +0900

    일단
    커밋

--- app/src/main/java/com/example/likedislike/MainActivity.java
+++ app/src/main/java/com/example/likedislike/MainActivity.java
@@ -20,7 +20,7 @@ public class MainActivity extends AppCompatActivity {
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Intent intent = new Intent(this,HomeActivity.class);
+        Intent intent = new Intent(getApplicationContext(),SignUpActivity.class);
         startActivity(intent);
         finish();
     }
